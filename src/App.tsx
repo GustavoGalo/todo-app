8b21ab5f3e546d9c2eeb5d7bdad136c001c2b12a
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-03-17 21:56:40 -0300

    setting up enviroment

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png"
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
